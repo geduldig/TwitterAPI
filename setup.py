3a10f8239cdc0523c5691d1ba2796d5a10f1aa9a
Jonas Geduldig <jonas.geduldig@abc.com>
2013-08-06 09:25:15 -0400

    TwitterRestPager does not exit when no tweets

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='TwitterAPI',
-    version='2.0.6',
+    version='2.0.7',
     author='Jonas Geduldig',
     author_email='boxnumber03@gmail.com',
     packages=['TwitterAPI'],
