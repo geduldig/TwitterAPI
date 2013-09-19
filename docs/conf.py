b7b458277c4100a7c9ecca5c910d9cc4aa10d611
geduldig <boxnumber03@gmail.com>
2013-09-19 12:34:06 -0400

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -34,7 +34,7 @@ class Mock(object):
             return Mock()
 MOCK_MODULES = ['requests', 'requests-oauthlib']
 for mod_name in MOCK_MODULES:
-    sys.modules[mod_name] = mock.Mock()
+    sys.modules[mod_name] = Mock()
 
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
