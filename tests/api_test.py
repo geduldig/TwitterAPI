9583e8a43db6ff9f9a5ace0afe64cf42bc9f5dd1
Jonas Geduldig <boxnumber03@gmail.com>
2013-09-23 22:27:36 -0400

    Added sphinx-generated docs

--- tests/api_test.py
+++ tests/api_test.py
@@ -7,10 +7,16 @@ o = TwitterOAuth.read_file()
 api = TwitterAPI(o.consumer_key, o.consumer_secret, o.access_token_key, o.access_token_secret)
 
 
-TEST_NUMBER = 2
+TEST_NUMBER = 0
 
 
 try:
+        if TEST_NUMBER == 0:
+
+                # VERIFY YOUR CREDS
+                r = api.request('account/verify_credentials')
+                print(r.text)
+
 	if TEST_NUMBER == 1:
 	
 		# POST A TWEET 
@@ -19,8 +25,8 @@ try:
 
 	if TEST_NUMBER == 2:
 	
-		# GET 20 TWEETS CONTAINING 'ZZZ'
-		for item in api.request('search/tweets', {'q':'zzz'}):
+		# GET 5 TWEETS CONTAINING 'ZZZ'
+		for item in api.request('search/tweets', {'q':'zzz', 'count':5}):
 			print(item['text'] if 'text' in item else item)
 
 	if TEST_NUMBER == 3:
@@ -32,7 +38,7 @@ try:
 	if TEST_NUMBER == 4:
 	
 		# GET TWEETS FROM THE PAST WEEK OR SO CONTAINING 'LOVE'
-		pager = TwitterRestPager(api, 'search/tweets', {'q':'love', 'count':5});	
+		pager = TwitterRestPager(api, 'search/tweets', {'q':'love'});	
 		for item in pager.get_iterator():
 			print(item['text'] if 'text' in item else item)
 			
