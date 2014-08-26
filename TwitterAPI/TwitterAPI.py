69fa875b64575cc0c31090c2c4de92ab2065c34c
Jonas Geduldig <jonas.geduldig@abc.com>
2014-08-26 10:34:31 -0400

    Fix POST request for query string

--- TwitterAPI/TwitterAPI.py
+++ TwitterAPI/TwitterAPI.py
@@ -92,11 +92,11 @@ class TwitterAPI(object):
             timeout = REST_SOCKET_TIMEOUT
         else:
             raise Exception('"%s" is not valid endpoint' % resource)
-        if method is 'GET':
-            data = None
-        elif method is 'POST':
+        if method is 'POST':
             data = params
             params = None
+        else:
+            data = None
         r = session.request(
             method,
             url,
