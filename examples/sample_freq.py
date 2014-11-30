c16216b416695e8270242afb3d9fd990f7d95d21
Jonas Geduldig <boxnumber03@gmail.com>
2014-11-30 16:54:21 -0500

    Added logging, error classes

--- examples/sample_freq.py
+++ examples/sample_freq.py
@@ -24,33 +24,33 @@ api = TwitterAPI(o.consumer_key,
 interval = 5
 
 while True:
-	logging.info('NEW REQUEST')
-	r = api.request('statuses/sample')
-	total_count = 0
-	total_start = time.time()
-	count = 0
-	start = total_start
-	try:
-		for item in r:
-			if 'text' in item:
-				count += 1
-				total_count += 1
-				#print(item['text'])
-			now = time.time()
-			elapsed = now-start
-			if elapsed >= interval:
-				print('%d\t%d\t%d' % (int(count/elapsed), 
-			                      	total_count,
-			                      	int(total_count/(now-total_start))))
-				start = now
-				count = 0
-	except TwitterError.TwitterConnectionError as e:
-		logging.info(type(e))
-		pass
-	except KeyboardInterrupt:
-		print('user terminated')
-		break
-	except Exception as e:
-		print(type(e))
-		print(e)
-		break
+    logging.info('NEW REQUEST')
+    r = api.request('statuses/sample')
+    total_count = 0
+    total_start = time.time()
+    count = 0
+    start = total_start
+    try:
+        for item in r:
+            if 'text' in item:
+                count += 1
+                total_count += 1
+                #print(item['text'])
+            now = time.time()
+            elapsed = now-start
+            if elapsed >= interval:
+                print('%d\t%d\t%d' % (int(count/elapsed), 
+                      total_count,
+                      int(total_count/(now-total_start))))
+                start = now
+                count = 0
+    except TwitterError.TwitterConnectionError as e:
+        logging.info(type(e))
+        pass
+    except KeyboardInterrupt:
+        print('user terminated')
+        break
+    except Exception as e:
+        print(type(e))
+        print(e)
+        break
