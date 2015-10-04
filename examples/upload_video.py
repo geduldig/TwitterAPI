cb75e14ececb64e0fd74e4b347e3d048b981f99d
Jonas Geduldig <boxnumber03@gmail.com>
2015-10-04 11:14:30 -0400

    Updated upload_video.py example

--- examples/upload_video.py
+++ examples/upload_video.py
@@ -1,23 +1,33 @@
+# Twitter places several restrictions on uploaded video. If your upload fails
+# with error 324, your video probably violates these restrictions.
+#
+# Read about them here:
+# https://dev.twitter.com/rest/public/uploading-media#videorecs
+
 from TwitterAPI import TwitterAPI
 import os
 import sys
 
+
 VIDEO_FILENAME = 'test.mp4'
 TWEET_TEXT = 'Video upload test'
-
-bytes_sent = 0
-total_bytes = os.path.getsize(VIDEO_FILENAME)
-video = open(VIDEO_FILENAME, 'rb')
 
 CONSUMER_KEY = ''
 CONSUMER_SECRET = ''
 ACCESS_TOKEN_KEY = ''
 ACCESS_TOKEN_SECRET = ''
 
+
 api = TwitterAPI(CONSUMER_KEY,
                  CONSUMER_SECRET,
                  ACCESS_TOKEN_KEY,
                  ACCESS_TOKEN_SECRET)
+
+
+bytes_sent = 0
+total_bytes = os.path.getsize(VIDEO_FILENAME)
+file = open(VIDEO_FILENAME, 'rb')
+
 
 def check_status(r):
 	if r.status_code < 200 or r.status_code > 299:
@@ -25,28 +35,23 @@ def check_status(r):
 		print(r.text)
 		sys.exit(0)
 
+
 r = api.request('media/upload', {'command':'INIT', 'media_type':'video/mp4', 'total_bytes':total_bytes})
-check_status(r.status_code)
+check_status(r)
 
 media_id = r.json()['media_id']
 segment_id = 0
 
-print ' [' + str(total_bytes) + '] ' + str(bytes_sent)
-
 while bytes_sent < total_bytes:
-  chunk = video.read(4*1024*1024)
-
+  chunk = file.read(4*1024*1024)
   r = api.request('media/upload', {'command':'APPEND', 'media_id':media_id, 'segment_index':segment_id}, {'media':chunk})
   check_status(r)
-
   segment_id = segment_id + 1
-  bytes_sent = video.tell()
-
-  print '[' + str(total_bytes) + '] ' + str(bytes_sent)
-  continue
+  bytes_sent = file.tell()
+  print('[' + str(total_bytes) + ']', str(bytes_sent))
 
 r = api.request('media/upload', {'command':'FINALIZE', 'media_id':media_id})
-check_status(r.status_code)
+check_status(r)
 
 r = api.request('statuses/update', {'status':TWEET_TEXT, 'media_ids':media_id})
-check_status(r.status_code)
+check_status(r)
