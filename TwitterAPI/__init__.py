ae13d2a06e20f099dd8776ea6089035ba9a39664
Jonas Geduldig <boxnumber03@gmail.com>
2015-10-28 11:41:54 -0400

    Removed deprecated classes

--- TwitterAPI/__init__.py
+++ TwitterAPI/__init__.py
@@ -1,5 +1,5 @@
 __title__ = 'TwitterAPI'
-__version__ = '2.3.4'
+__version__ = '2.3.5'
 __author__ = 'Jonas Geduldig'
 __license__ = 'MIT'
 __copyright__ = 'Copyright 2013 Jonas Geduldig'
@@ -13,7 +13,7 @@ logging.getLogger(__name__).addHandler(logging.NullHandler())
 
 
 try:
-    from .TwitterAPI import TwitterAPI, TwitterResponse, RestIterator, StreamingIterator
+    from .TwitterAPI import TwitterAPI, TwitterResponse
     from .TwitterError import TwitterConnectionError, TwitterRequestError
     from .TwitterOAuth import TwitterOAuth
     from .TwitterRestPager import TwitterRestPager
