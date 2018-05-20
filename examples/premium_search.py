62410d10f04d825ff55a9c46ab8e16d1c879eadf
Jonas Geduldig <boxnumber03@gmail.com>
2018-05-20 10:32:16 -0400

    Forgot to add premium_search.py to git

---
+++ examples/premium_search.py
@@ -0,0 +1,17 @@
+from TwitterAPI import TwitterAPI
+
+
+SEARCH_TERM = 'pizza'
+PRODUCT = '30day'
+LABEL = 'your label'
+
+api = TwitterAPI(<consumer key>, 
+                 <consumer secret>,
+                 <access token key>,
+                 <access token secret>)
+
+r = api.request('tweets/search/%s/:%s' % (PRODUCT, LABEL), 
+               {'query':SEARCH_TERM})
+
+for item in r:
+    print(item['text'] if 'text' in item else item)
