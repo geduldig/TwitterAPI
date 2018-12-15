f4a99ff2bd3962424740bf5dd6915d8aa9258daf
Jonas Geduldig <boxnumber03@gmail.com>
2018-12-15 09:06:13 -0500

    Merged HyperManTT patch

--- TwitterAPI/constants.py
+++ TwitterAPI/constants.py
@@ -208,10 +208,10 @@ ENDPOINTS = {
     # NEW ACCOUNT ACTIVITY WEBHOOK API (BETA)
 
     'account_activity/all/:PARAM/subscriptions':            ('POST',   'api'), # ENVIRONMENT NAME
+    'account_activity/all/:PARAM/subscriptions':            ('DELETE', 'api'), # ENVIRONMENT NAME                            
     'account_activity/all/:PARAM/subscriptions/all':        ('GET',    'api'), # ENVIRONMENT NAME
     'account_activity/all/:PARAM/subscriptions/all/list':   ('GET',    'api'), # ENVIRONMENT NAME
     'account_activity/all/:PARAM/subscriptions/list':       ('GET',    'api'), # ENVIRONMENT NAME
-    'account_activity/all/:PARAM/subscriptions':            ('DELETE', 'api'), # ENVIRONMENT NAME                            
     'account_activity/all/:PARAM/webhooks':                 ('POST',   'api'), # ENVIRONMENT NAME    
     'account_activity/all/:PARAM/webhooks/:PARAM':          ('DELETE', 'api'), # ENVIRONMENT NAME, WEBHOOK ID
     'account_activity/all/count':                           ('GET',    'api'), 
